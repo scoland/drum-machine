decfad0cd8d41b19eae0c538afadb5826dd5e397
Sean Colandrea <link50@gmail.com>
2016-05-17 11:13:37 -0400

    Got the pad flashing functionality down

--- app/components/pad/pad.js
+++ app/components/pad/pad.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import classNames from 'classnames';
 require('./pad.scss');
 
 class Pad extends React.Component {
   render() {
+    console.log(this.props);
+    const isOn = this.props.isOn ? 'hit' : '';
+    const classes = classNames('pad', isOn);
+    console.log(classes);
     return (
-      <div className="pad">
+      <div className={classes}>
         {this.props.step}
       </div>
     );
--- app/index.js
+++ app/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pad from './components/pad/pad';
+import Tone from 'tone';
 require('./app.scss');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      matrix: [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+      currentNote: null
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.state.matrix);
+    const seq = new Tone.Sequence((time, note) => {
+      this.setState({
+        currentNote: note
+      })
+    }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "8n");
+      Tone.Transport.start();
+      seq.start();
+    }
+
   render() {
     const pads = [];
     for (var i = 0; i < 16; i++) {
-      pads.push(<Pad step={i}/>)
+      pads.push(<Pad step={i} isOn={this.state.currentNote === i}/>)
     }
 
     return (
